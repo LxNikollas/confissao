2c50e316f4e968e148aecfaaedf7304e08236380
Lexyu <103967043+LxNikollas@users.noreply.github.com>
2022-10-07 10:43:50 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "Euuu Te Amo",
+    introTitle: 'Letícia',
+    introDesc: `Você é a minha casa, minha lua, meu sol, minhas estrelas, você é meu mundo, meu universo, onde tudo começa e onde para sempre vou ficar.`,
+    btnIntro: '^^HiHi^^',
+    title: 'Você se apaixonou à primeira vista🥰',
+    desc: 'Eu fiquei bêbado desde o momento em que vi aquele sorriso!',
+    quest: 'Você gosta de mim?',
+    btnYes: 'Eu gosto <33',
+    btnNo: 'Não :3',
+    question: 'No mundo mais de 7 bilhões de pessoas, por que você me ama <3',
+    btnReply: 'Envie para MIM <3',
+    reply: 'Sim',
+    mess: 'Eu sei 🥰.Te amo muito😘😘',
+    messDesc: 'Segunda feira vou fazer que seja Incrível!',
+    btnAccept: 'Clique aqui.',
+    messLink: 'https://www.instagram.com/nikollas.gregory2104' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
